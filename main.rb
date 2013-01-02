c4a780bfab2f0ef1118d1a87a67ead1aa0e0b99a
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-01-02 21:41:28 +0200

    json, temporary removed memcache, formating data

--- main.rb
+++ main.rb
@@ -1,58 +1,48 @@
 # coding: UTF-8
 
 require 'sinatra'
-require 'dalli'
-require 'memcachier'
+#require 'dalli'
+#require 'memcachier'
+require 'json'
+require 'geocoder'
+require 'mechanize'
 
-set :cache, Dalli::Client.new
+# prepare memcache client
+#set :cache, Dalli::Client.new
 
+# prepare geocoder
+Geocoder.configure(:lookup => :yandex, :language => :ru)
+
+# getting current information
 def get_info
 
-  require 'mechanize'
-
   req = Mechanize.new.get 'http://transit.in.ua/importTransport.php?dataRequest%5B%5D=dnepropetrovsk-taxi-20&dataRequest%5B%5D=dnepropetrovsk-taxi-33&dataRequest%5B%5D=dnepropetrovsk-taxi-127'
-  settings.cache.set('data', req.body)
 
-end
+  data = []
 
-get '/' do
-
-  # remove later
-  get_info
+  Geocoder.configure(:lookup => :yandex, :language => :ru)
   
-  require 'geocoder'
+  (JSON.load req.body).each do |raw_way|
 
-  Geocoder.configure(
+    place = Geocoder.search((raw_way['cordinate'].to_s)[1..-2]).first.data['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
 
-    :lookup => :yandex,
-    :language => :ru
+    # formating place
+    place = place.split(',')[4..-1].join(',')
 
-  )
-
-  data = ''
-
-  require 'json'
-
-  (JSON.load settings.cache.get('data')).each do |one_way|
-
-    place_geo = Geocoder.search((one_way['cordinate'].to_s)[1..-2])
-
-    place = place_geo.first.data['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
-
-    # for google
-    #place = place_geo.first.data['address_components'][1]['short_name'] + ', ' + place_geo.first.data['address_components'][0]['short_name']
-
-    data = data + one_way['info'] + '<br>' + place + '<br><br>'
+    data << ({:info => raw_way['info'], :place => place})
 
   end
+
+  #settings.cache.set('data', data)
 
   data
 
 end
 
-get '/fetch' do
 
-  get_info
+get '/' do
+
+  get_info.to_json
 
 end
 
