d9093b86ef149f1ad41cc0b058a27b59a66c8102
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-01-02 16:11:26 +0200

    back to google geolocation

--- main.rb
+++ main.rb
@@ -24,11 +24,8 @@ get '/' do
 
   Geocoder.configure(
 
-    :api_key => 'AIzaSyAj92jkzuo0TVm_gCNz7dto3WwEt5j3wbo',
-    :lookup => :google,
-    :language => :ru,
-    :timeout => 5,
-    :units => :m
+    :lookup => :yandex,
+    :language => :ru
 
   )
 
@@ -40,10 +37,10 @@ get '/' do
 
     place_geo = Geocoder.search((one_way['cordinate'].to_s)[1..-2])
 
-    # for yandex
-    #place = place_geo.first.data['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
+    place = place_geo.first.data['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
 
-    place = place_geo.first.data['address_components'][1]['short_name'] + ', ' + place_geo.first.data['address_components'][0]['short_name']
+    # for google
+    #place = place_geo.first.data['address_components'][1]['short_name'] + ', ' + place_geo.first.data['address_components'][0]['short_name']
 
     data = data + one_way['info'] + '<br>' + place + '<br><br>'
 
