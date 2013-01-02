0cb4a3bd0c387f3af4697819fd70fe3bb8e7b949
Aleksey Lashin <aleksey.lashin@gmail.com>
2013-01-02 13:01:28 +0200

    added memcache, info on frontpage

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source "https://rubygems.org"
 
 gem 'sinatra'
 gem 'mechanize'
+gem 'dalli'
+gem 'json'
 
 group :development do
   gem 'shotgun'
--- main.rb
+++ main.rb
@@ -1,19 +1,32 @@
 # coding: UTF-8
 
 require 'sinatra'
-require 'mechanize'
+require 'dalli'
+
+set :cache, Dalli::Client.new
 
 get '/' do
+  
+  require 'json'
 
-  return '<h1>Page</h1>'
+  data = ''
+
+  (JSON.load settings.cache.get('data')).each do |one_way|
+
+    data = data + one_way['info'] + '<br>'
+
+  end
+
+  data
 
 end
 
 get '/fetch' do
 
-  a = Mechanize.new
-  req = a.get 'http://transit.in.ua/importTransport.php?dataRequest%5B%5D=dnepropetrovsk-taxi-20&dataRequest%5B%5D=dnepropetrovsk-taxi-33&dataRequest%5B%5D=dnepropetrovsk-taxi-127'
-  return req.body
+  require 'mechanize'
+
+  req = Mechanize.new.get 'http://transit.in.ua/importTransport.php?dataRequest%5B%5D=dnepropetrovsk-taxi-20&dataRequest%5B%5D=dnepropetrovsk-taxi-33&dataRequest%5B%5D=dnepropetrovsk-taxi-127'
+  settings.cache.set('data', req.body)
 
 end
 
