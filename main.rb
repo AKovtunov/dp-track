2f797188e543049dcb5ca7ae5dc8493275c405ab
AKovtunov <1sasha.sasha1@gmail.com>
2013-09-30 10:56:43 +0300

    Small review

--- main.rb
+++ main.rb
@@ -9,7 +9,8 @@ require 'mechanize'
 
 # prepare memcache client
 set :cache, Dalli::Client.new
-
+@raw_data_stirng
+@data_string
 # prepare geocoder
 Geocoder.configure(:lookup => :yandex, :language => :ru)
 
@@ -49,11 +50,23 @@ def get_info  (ways)
 end
 
 def test
+  @data_string = []
   url = 'http://transit.in.ua/importTransport.php?'
   url = url + 'dataRequest%5B%5D=dnepropetrovsk-taxi-'+127.to_s+'&'
   req = Mechanize.new.get url
   Geocoder.configure(:lookup => :yandex, :language => :ru)
-  JSON.load req.body
+  @raw_data_stirng = JSON.load req.body
+  (@raw_data_stirng).each do |raw_way|
+
+    place = Geocoder.search((raw_way['cordinate'].to_s)[1..-2]).first.data['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']
+
+    # formating place
+    place = place.split(',')
+
+    @data_string << ({:info => raw_way['info'], :place => place})
+
+  end
+  @data_string
 end
 # fetch info
 get '/fetch' do
@@ -63,6 +76,7 @@ get '/fetch' do
 end
 get '/test' do
 
+  
   test.to_json
 
 end
